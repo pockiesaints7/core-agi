3719c69a7dd38b39a07a162e53409bc86ff8e053
pockiesaints7 <pockiesaints7@gmail.com>
2026-03-15 03:38:40 +0700

    TASK-16.A: import SUPABASE_REF into core_tools.py

--- core_tools.py
+++ core_tools.py
@@ -22,7 +22,7 @@ import httpx
 from core_config import (
     GITHUB_REPO, GROQ_FAST, GROQ_MODEL, KB_MINE_BATCH_SIZE, KB_MINE_RATIO_THRESHOLD,
     COLD_HOT_THRESHOLD, COLD_KB_GROWTH_THRESHOLD, PATTERN_EVO_THRESHOLD,
-    KNOWLEDGE_AUTO_CONFIDENCE, MCP_PROTOCOL_VERSION, SUPABASE_URL,
+    KNOWLEDGE_AUTO_CONFIDENCE, MCP_PROTOCOL_VERSION, SUPABASE_URL, SUPABASE_REF,
     L, groq_chat, sb_get, sb_post, sb_post_critical, sb_patch, sb_upsert, sb_delete,
 )
 from core_config import _sbh, _sbh_count_svc
